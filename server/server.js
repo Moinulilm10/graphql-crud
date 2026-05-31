2949590bb226cd318056b4fd429836fcf3bce59f
Moinulilm10 <moinulilm10@gmail.com>
2026-06-01 00:21:29 +0600

    Add GraphQL type definitions for User queries and mutations

--- server/server.js
+++ server/server.js
@@ -1,6 +1,27 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+const typeDefs = `#graphql
+  type Query {
+    getUsers: [User]
+    getUserById(id: ID!): User
+  }
+
+  type Mutation {
+    createUser(name: String!, age: Int, isMarried: Boolean): User
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    age: Int
+    isMarried: Boolean
+  }
+
+
+
+`;
+
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const { url } = await startStandaloneServer(server, {
